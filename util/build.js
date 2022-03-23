1c3fd94d85b68fd61c00ea6b759ae2c09ba3561c
dnialwill <25089409+dnialwill@users.noreply.github.com>
2022-03-23 19:12:40 -0400

    extract build scripts

--- util/build.js
+++ util/build.js
@@ -1,42 +1,7 @@
-const fs = require("fs").promises;
+const { createDescriptorModFiles } = require("./create-descriptor-mod-files");
 
 build();
 
 async function build() {
   await createDescriptorModFiles();
-}
-
-async function createDescriptorModFiles() {
-  console.log("Building app...");
-
-  try {
-    const packageJson = require("../package.json");
-    let { ck3: metadata, version } = packageJson;
-    metadata = { ...metadata, version };
-
-    if (!metadata) throw new Error("No metadata in package.json!");
-
-    const descriptorLines = [];
-
-    for (const key in metadata) {
-      const value = metadata[key];
-
-      const line = !Array.isArray(value)
-        ? `${key}="${value}"`
-        : `${key}={\r\n${value.map((val) => `\t"${val}"`).join("\r\n")}\r\n}`;
-
-      descriptorLines.push(line);
-    }
-
-    const descriptorText = descriptorLines.join("\r\n");
-
-    await fs.mkdir("./dist");
-    await fs.writeFile(`./dist/${metadata.name}.mod`, descriptorText);
-    await fs.mkdir(`./dist/${metadata.name}`);
-    await fs.writeFile(`./dist/${metadata.name}/descriptor.mod`, descriptorText);
-  } catch (e) {
-    throw new Error("An error occurred: " + e.message);
-  }
-
-  console.log("Build complete!");
 }
---
+++ util/create-descriptor-mod-files.js
@@ -0,0 +1,41 @@
+const { promises: fs } = require("fs");
+
+async function createDescriptorModFiles() {
+  console.log("Building app...");
+
+  try {
+    const packageJson = require("../package.json");
+    let { ck3: metadata, version } = packageJson;
+    metadata = { ...metadata, version };
+
+    if (!metadata) throw new Error("No metadata in package.json!");
+
+    const descriptorLines = [];
+
+    for (const key in metadata) {
+      const value = metadata[key];
+
+      const line = !Array.isArray(value)
+        ? `${key}="${value}"`
+        : `${key}={\r\n${value.map((val) => `\t"${val}"`).join("\r\n")}\r\n}`;
+
+      descriptorLines.push(line);
+    }
+
+    const descriptorText = descriptorLines.join("\r\n");
+
+    await fs.mkdir("./dist");
+    await fs.writeFile(`./dist/${metadata.name}.mod`, descriptorText);
+    await fs.mkdir(`./dist/${metadata.name}`);
+    await fs.writeFile(
+      `./dist/${metadata.name}/descriptor.mod`,
+      descriptorText
+    );
+  } catch (e) {
+    throw new Error("An error occurred: " + e.message);
+  }
+
+  console.log("Build complete!");
+}
+
+module.exports = { createDescriptorModFiles };
